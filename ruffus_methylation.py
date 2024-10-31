77b257a1ed0725b455faa16e5a71a89fe89d82d8
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-10-31 18:00:32 +0000

    Cleanup trimming in pipeline

--- ruffus_methylation.py
+++ ruffus_methylation.py
@@ -9,7 +9,6 @@ import multiprocessing
 import glob
 from ruffus import *
 from ruffus import formatter as ruffus_formatter
-# import logging
 import sys
 import shutil
 import argparse
@@ -48,10 +47,11 @@ def setup_output_dirs():
 
 def get_input_files():
     """Get input files based on test flag"""
-    if OPTIONS.test:
-        return glob.glob("fastq/test*_1.fastq.gz")
-    return glob.glob("fastq/*_1.fastq.gz")
+    # if OPTIONS.test:
+    return (glob.glob("fastq/test*_1.fastq.gz")[0],glob.glob("fastq/test*_2.fastq.gz")[0])
+    # return glob.glob("fastq/*_1.fastq.gz")
 
+input_files = get_input_files()
 
 # NOTE: this part of the pipeline hasn't been run so far. I had problems with the regex for the files
 # @transform(glob.glob("fastq/*_1.fastq.gz") if not (OPTIONS and OPTIONS.test)
@@ -60,16 +60,18 @@ def get_input_files():
 #           [os.path.join(PARAMS['output_dir'], "trimmed/initial", "{SID[0]}_1_val_1.fq.gz"),
 #            os.path.join(PARAMS['output_dir'], "trimmed/initial", "{SID[0]}_2_val_2.fq.gz")],
 #            "{SID[0]}")
-@transform(get_input_files,
-          regex(r"fastq/(.+)_1.fastq.gz"),
-          [os.path.join(PARAMS['output_dir'], "trimmed/initial", r"\1_1_val_1.fq.gz"),
-           os.path.join(PARAMS['output_dir'], "trimmed/initial", r"\1_2_val_2.fq.gz")],
-          r"\1")
-def initial_trim(input_r1, output_files, sample_name):
+@transform([input_files],# ["fastq/test*_1.fastq.gz", "fastq/test*_2.fastq.gz"],
+          ruffus_formatter("fastq/(?P<SID>[^/]+)_[12].fastq.gz"),
+          [os.path.join(PARAMS['output_dir'], "trimmed/initial", "{SID[0]}_1_val_1.fq.gz"),
+           os.path.join(PARAMS['output_dir'], "trimmed/initial", "{SID[0]}_2_val_2.fq.gz")],
+          "{SID[0]}")
+def initial_trim(input_files, output_files, sample_name):
     """Initial trimming step"""
     logger.info(f"Starting initial trimming for sample: {sample_name}")
 
-    input_r2 = input_r1.replace("_1.fastq.gz", "_2.fastq.gz")
+    # input_r2 = input_r1.replace("_1.fastq.gz", "_2.fastq.gz")
+    input_r1 = input_files[0]
+    input_r2 = input_files[1]
     if not os.path.exists(input_r2):
         raise ValueError(f"Missing R2 file for {input_r1}")
 
@@ -95,64 +97,6 @@ def initial_trim(input_r1, output_files, sample_name):
 @follows(initial_trim)
 @transform(initial_trim,
           formatter(),
-          [os.path.join(PARAMS['output_dir'], "trimmed/poly", "{basename[0]}_1_val_1.fq.gz"),
-           os.path.join(PARAMS['output_dir'], "trimmed/poly", "{basename[0]}_2_val_2.fq.gz")])
-def poly_trim(input_files, output_files):
-    """PolyA trimming step"""
-    # NOTE: we might still want to save polyA sequences (like in this example:)
-    # PLEASE NOTE: The poly-A trimming mode expects that sequences were both adapter and quality trimmed
-    # before looking for Poly-A tails, and it is the user's responsibility to carry out an initial round of
-    # trimming.
-
-    # FIXME: we don't know why we have so many polyA sequences in our data! (check why)
-
-    input_r1, input_r2 = input_files
-    output_r1, output_r2 = output_files
-    sample_name = os.path.basename(input_r1).split('_')[0]
-
-    logger.info(f"Starting polyA trimming for sample: {sample_name}")
-
-    poly_cmd = (
-        f"trim_galore --paired --polyA "
-        f"--output_dir {os.path.dirname(output_r1)} "
-        f"{input_r1} {input_r2}"
-    )
-
-    try:
-        subprocess.run(poly_cmd, shell=True, check=True)
-        logger.info(f"Completed polyA trimming for sample: {sample_name}")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error in poly_trim for {sample_name}: {str(e)}")
-        raise
-
-@follows(poly_trim)
-@transform(poly_trim,
-          formatter(),
-          [os.path.join(PARAMS['output_dir'], "trimmed/polyT", "{basename[0]}_1_val_1.fq.gz"),
-           os.path.join(PARAMS['output_dir'], "trimmed/polyT", "{basename[0]}_2_val_2.fq.gz")])
-def polyT_trim(input_files, output_files):
-    """PolyT trimming step using cutadapt"""
-    input_r1, input_r2 = input_files
-    output_r1, output_r2 = output_files
-    sample_name = os.path.basename(input_r1).split('_')[0]
-
-    logger.info(f"Starting polyT trimming for sample: {sample_name}")
-
-    cmd = (
-        f"cutadapt -a 'T{{10}}' -A 'T{{10}}' -g 'T{{10}}' -G 'T{{10}}' -m 20 "
-        f"-o {output_r1} -p {output_r2} {input_r1} {input_r2}"
-    )
-
-    try:
-        subprocess.run(cmd, shell=True, check=True)
-        logger.info(f"Completed polyT trimming for sample: {sample_name}")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error in polyT_trim for {sample_name}: {str(e)}")
-        raise
-
-@follows(polyT_trim)
-@transform(polyT_trim,
-          formatter(),
           [os.path.join(PARAMS['output_dir'], "trimmed/polyA", "{basename[0]}_1_val_1.fq.gz"),
            os.path.join(PARAMS['output_dir'], "trimmed/polyA", "{basename[0]}_2_val_2.fq.gz")])
 def polyA_trim(input_files, output_files):
@@ -163,8 +107,10 @@ def polyA_trim(input_files, output_files):
 
     logger.info(f"Starting polyA trimming for sample: {sample_name}")
 
+    # NOTE: we hope we can do the cutadapt in a single pass like this
     cmd = (
         f"cutadapt -a 'A{{10}}' -A 'A{{10}}' -g 'A{{10}}' -G 'A{{10}}' -m 20 "
+        f"-a 'T{{10}}' -A 'T{{10}}' -g 'T{{10}}' -G 'T{{10}}' "
         f"-o {output_r1} -p {output_r2} {input_r1} {input_r2}"
     )
 
@@ -177,6 +123,32 @@ def polyA_trim(input_files, output_files):
 
 @follows(polyA_trim)
 @transform(polyA_trim,
+          formatter(),
+          [os.path.join(PARAMS['output_dir'], "trimmed/clean", "{basename[0]}_1.fq.gz"),
+           os.path.join(PARAMS['output_dir'], "trimmed/clean", "{basename[0]}_2.fq.gz")])
+def cleanup_names(input_files, output_files):
+    """Clean up the file names after all trimming steps"""
+    input_r1, input_r2 = input_files
+    output_r1, output_r2 = output_files
+    sample_name = os.path.basename(input_r1).split('_')[0]
+
+    # Create clean directory if it doesn't exist
+    os.makedirs(os.path.dirname(output_r1), exist_ok=True)
+
+    logger.info(f"Cleaning up file names for sample: {sample_name}")
+
+    try:
+        # Simply copy files with clean names
+        shutil.copy2(input_r1, output_r1)
+        shutil.copy2(input_r2, output_r2)
+        logger.info(f"Completed filename cleanup for sample: {sample_name}")
+    except Exception as e:
+        logger.error(f"Error in cleanup_names for {sample_name}: {str(e)}")
+        raise
+
+
+@follows(cleanup_names)
+@transform(cleanup_names,
           formatter(),
           os.path.join(PARAMS['output_dir'], "aligned", "{basename[0]}_bismark_bt2_pe.bam"))
 def align_bismark(input_files, output_file):
@@ -206,7 +178,7 @@ def align_bismark(input_files, output_file):
         f"--output_dir {output_dir} "
         f"--genome {PARAMS['reference_genome']} "
         f"-1 {trimmed1} -2 {trimmed2} "
-        f"--score_min L,-0.6,-1.0 --non_directional"
+        f"--score_min L,-0.6,-1.0 --non_directional --unmapped {output_dir}unmapped_{sample_name}"
     )
 
     try:
@@ -233,9 +205,9 @@ def main():
     try:
         # Run the pipeline
         pipeline_run(
-            target_tasks=[align_bismark],
+            # target_tasks=[align_bismark],
             # multiprocess=PARAMS["max_parallel_jobs"], #NOTE: not sure this is needed
-            # **pipeline_options
+            **pipeline_options
         )
     except Exception as e:
         logger.error(f"Pipeline failed: {str(e)}")
@@ -243,8 +215,8 @@ def main():
         raise
 
 if __name__ == "__main__":
-    # try:
-    main()
-    # except Exception as e:
-    #     logging.error(f"Pipeline execution failed: {str(e)}")
-    #     raise
+    try:
+        main()
+    except Exception as e:
+        logging.error(f"Pipeline execution failed: {str(e)}")
+        raise
