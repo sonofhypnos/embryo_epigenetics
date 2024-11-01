183508d1d31f41ec485c295b72c2dee0b2de3848
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-11-01 10:07:38 +0000

    Simplify pipeline

--- ruffus_methylation.py
+++ ruffus_methylation.py
@@ -10,14 +10,16 @@ import glob
 from ruffus import *
 from ruffus import formatter as ruffus_formatter
 import sys
-import shutil
 import argparse
 import logging
+import shutil
 
 def parse_arguments():
     """Parse command line arguments"""
     parser = argparse.ArgumentParser(description='RNA-seq analysis pipeline')
     parser.add_argument('--test', action='store_true',
+                       help='Run pipeline on test files only (files starting with "test" in /fastq/)')
+    parser.add_argument('--test2', action='store_true',
                        help='Run pipeline on test files only (files starting with "test" in /fastq/)')
     parser.add_argument('--log', default='pipeline.log',
                        help='Log file path (default: pipeline.log)')
@@ -51,6 +53,8 @@ def get_input_files():
     if OPTIONS.test:
         return [(glob.glob("fastq/test*_1.fastq.gz")[0],glob.glob("fastq/test*_2.fastq.gz")[0])]
 
+    if OPTIONS.test2:
+        return [("fastq/SRR5720828_1.fastq.gz", "fastq/SRR5720828_2.fastq.gz")]
     r1_files = glob.glob("fastq/*_1.fastq.gz")
     # For each R1 file, identify the corresponding R2 file by replacing "_1.fastq.gz" with "_2.fastq.gz"
     pairs = [(r1, r1.replace("_1.fastq.gz", "_2.fastq.gz")) for r1 in r1_files if os.path.exists(r1.replace("_1.fastq.gz", "_2.fastq.gz"))]
@@ -100,38 +104,38 @@ def initial_trim(input_files, output_files, sample_name):
         logger.error(f"Error in initial_trim for {sample_name}: {str(e)}")
         raise
 
+# @follows(initial_trim)
+# @transform(initial_trim,
+#           formatter(),
+#           [os.path.join(PARAMS['output_dir'], "trimmed/polyA", "{basename[0]}_1_val_1.fq.gz"),
+#            os.path.join(PARAMS['output_dir'], "trimmed/polyA", "{basename[0]}_2_val_2.fq.gz")])
+# def polyA_trim(input_files, output_files):
+#     """PolyA trimming step using cutadapt"""
+#     input_r1, input_r2 = input_files
+#     output_r1, output_r2 = output_files
+#     sample_name = os.path.basename(input_r1).split('_')[0]
+
+#     logger.info(f"Starting polyA trimming for sample: {sample_name}")
+
+#     # NOTE: we hope we can do the cutadapt in a single pass like this
+#     cmd = (
+#         f"cutadapt -a 'A{{10}}' -A 'A{{10}}' -g 'A{{10}}' -G 'A{{10}}' -m 20 "
+#         f"-a 'T{{10}}' -A 'T{{10}}' -g 'T{{10}}' -G 'T{{10}}' "
+#         f"-o {output_r1} -p {output_r2} {input_r1} {input_r2}"
+#     )
+
+#     try:
+#         subprocess.run(cmd, shell=True, check=True)
+#         logger.info(f"Completed polyA trimming for sample: {sample_name}")
+#     except subprocess.CalledProcessError as e:
+#         logger.error(f"Error in polyA_trim for {sample_name}: {str(e)}")
+#         raise
+
 @follows(initial_trim)
 @transform(initial_trim,
-          formatter(),
-          [os.path.join(PARAMS['output_dir'], "trimmed/polyA", "{basename[0]}_1_val_1.fq.gz"),
-           os.path.join(PARAMS['output_dir'], "trimmed/polyA", "{basename[0]}_2_val_2.fq.gz")])
-def polyA_trim(input_files, output_files):
-    """PolyA trimming step using cutadapt"""
-    input_r1, input_r2 = input_files
-    output_r1, output_r2 = output_files
-    sample_name = os.path.basename(input_r1).split('_')[0]
-
-    logger.info(f"Starting polyA trimming for sample: {sample_name}")
-
-    # NOTE: we hope we can do the cutadapt in a single pass like this
-    cmd = (
-        f"cutadapt -a 'A{{10}}' -A 'A{{10}}' -g 'A{{10}}' -G 'A{{10}}' -m 20 "
-        f"-a 'T{{10}}' -A 'T{{10}}' -g 'T{{10}}' -G 'T{{10}}' "
-        f"-o {output_r1} -p {output_r2} {input_r1} {input_r2}"
-    )
-
-    try:
-        subprocess.run(cmd, shell=True, check=True)
-        logger.info(f"Completed polyA trimming for sample: {sample_name}")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error in polyA_trim for {sample_name}: {str(e)}")
-        raise
-
-@follows(polyA_trim)
-@transform(polyA_trim,
-          formatter(),
-          [os.path.join(PARAMS['output_dir'], "trimmed/clean", "{basename[0]}_1.fq.gz"),
-           os.path.join(PARAMS['output_dir'], "trimmed/clean", "{basename[0]}_2.fq.gz")])
+          formatter(".+/([^/_]+)_.*\.fq\.gz"),  # Captures everything before first underscore
+          [os.path.join(PARAMS['output_dir'], "trimmed/clean", "{1[0]}_1.fq.gz"),
+           os.path.join(PARAMS['output_dir'], "trimmed/clean", "{1[0]}_2.fq.gz")])
 def cleanup_names(input_files, output_files):
     """Clean up the file names after all trimming steps"""
     input_r1, input_r2 = input_files
@@ -145,60 +149,116 @@ def cleanup_names(input_files, output_files):
 
     try:
         # Simply copy files with clean names
-        shutil.copy2(input_r1, output_r1)
-        shutil.copy2(input_r2, output_r2)
+        shutil.move(input_r1, output_r1)
+        shutil.move(input_r2, output_r2)
         logger.info(f"Completed filename cleanup for sample: {sample_name}")
     except Exception as e:
         logger.error(f"Error in cleanup_names for {sample_name}: {str(e)}")
         raise
 
 
+@jobs_limit(8)
 @follows(cleanup_names)
 @transform(cleanup_names,
-          formatter(),
-          os.path.join(PARAMS['output_dir'], "aligned", "{basename[0]}_bismark_bt2_pe.bam"))
-def align_bismark(input_files, output_file):
+          formatter(".+/([^/_]+)_.*\.fq\.gz"),  # Captures everything before first underscore
+          [
+           # os.path.join(PARAMS['output_dir'], "aligned", "{1[0]}_bismark_bt2_pe.bam"),
+           # os.path.join(PARAMS['output_dir'], "aligned", "{1[0]}_2_bismark_bt2_pe.bam"),
+           os.path.join(PARAMS['output_dir'], "aligned", "{1[0]}_1.fq.gz_unmapped_reads_1.fq.gz"),
+           os.path.join(PARAMS['output_dir'], "aligned", "{1[0]}_2.fq.gz_unmapped_reads_2.fq.gz")])
+def align_bismark(input_files, output_files):
     """Align reads using Bismark"""
-    # TODO: add really simple tests for some of these steps
-    # NOTE: it seems like making a custom index for our genes is in fact not that much faster?
-    # (we do need less memory-> we can go with a higher number of cores)
-    # (seems like we were in fact still using entire chromosomes and this is why it took that long!)
-    # TODO: the bismark reference recommends a deduplication step for full genome
-    # sequencing (not for RRBS). Learn what that is useful for? (and then add it to our pipeline)
-    # NOTE: we do not know why, but testing on a small subset of the dataset
-    # revealed using non-directional mapping with bismark to be if anything better on our dataset
-    # (less bias in matching sequences and higher mapq scores)
-    # TODO: Do further quality control on all our sequences (so far I only did quality control on small samples)
-    # TODO: Add befile step
+     # TODO: add really simple tests for some of these steps
+     # NOTE: it seems like making a custom index for our genes is in fact not that much faster?
+     # (we do need less memory-> we can go with a higher number of cores)
+     # (seems like we were in fact still using entire chromosomes and this is why it took that long!)
+     # TODO: the bismark reference recommends a deduplication step for full genome
+     # sequencing (not for RRBS). Learn what that is useful for? (and then add it to our pipeline)
+     # NOTE: we do not know why, but testing on a small subset of the dataset
+     # revealed using non-directional mapping with bismark to be if anything better on our dataset
+     # (less bias in matching sequences and higher mapq scores)
+     # TODO: Do further quality control on all our sequences (so far I only did quality control on small samples)
+     # TODO: Add bedfile step
+     # TODO: perform deduplicate_bismark (to remove duplicates from pcr
+     # amplification for whole genome sequencing) I think there weren't too many
+     # duplicates, so this shouldn't be bad to skip.
 
     trimmed1, trimmed2 = input_files
+    #output_bam,
+    output_unmapped1, output_unmapped2 = output_files
     output_dir = os.path.join(PARAMS['output_dir'], "aligned")
     sample_name = os.path.basename(trimmed1).split('_')[0]
 
     logger.info(f"Starting alignment for sample: {sample_name}")
 
-    # We chose the score for score_min, because this value still gave sequences where the majority 75%
-    # of errors was due to C->T or A->G errors
     cmd = (
         f"bismark "
         f"--output_dir {output_dir} "
         f"--genome {PARAMS['reference_genome']} "
         f"-1 {trimmed1} -2 {trimmed2} "
-        f"--score_min L,-0.6,-1.0 --non_directional --unmapped {output_dir}unmapped_{sample_name}"
+        f"--non_directional --unmapped"
     )
 
     try:
         subprocess.run(cmd, shell=True, check=True)
         # Move the output BAM file to the expected location
-        bam_file = os.path.join(output_dir,
-                               os.path.basename(trimmed1).replace('_1_val_1.fq.gz', '_bismark_bt2_pe.bam'))
-        if os.path.exists(bam_file):
-            os.rename(bam_file, output_file)
+        # bam_file = os.path.join(output_dir,
+        #                        os.path.basename(trimmed1).replace('_1.fq.gz', '_bismark_bt2_pe.bam'))
+        # if os.path.exists(bam_file):
+        #     os.rename(bam_file, output_bam)
 
-        logger.info(f"Completed alignment for sample: {sample_name}")
+        # # Ensure unmapped files are in the expected location
+        # for i, unmapped_file in enumerate([output_unmapped1, output_unmapped2], 1):
+        #     src = os.path.join(output_dir, f"{os.path.basename(trimmed1).replace('_1.fq.gz', '')}_{i}.fastq_unmapped_reads_{i}.fq.gz")
+        #     if os.path.exists(src):
+        #         os.rename(src, unmapped_file)
+
+        # logger.info(f"Completed alignment for sample: {sample_name}")
     except subprocess.CalledProcessError as e:
         logger.error(f"Error in align_bismark for {sample_name}: {str(e)}")
         raise
+
+@jobs_limit(8)
+@follows(align_bismark)
+@transform(align_bismark,
+           formatter(),
+          [os.path.join(PARAMS['output_dir'], "aligned", "{basename[0]}_unmapped1_bt2.bam"),
+           os.path.join(PARAMS['output_dir'], "aligned", "{basename[0]}_unmapped2_bt2.bam")])
+def align_unmapped(input_files, output_files):
+    """Align unmapped reads individually using Bismark"""
+    unmapped1, unmapped2 = input_files
+    output_bam1, output_bam2 = output_files
+    sample_name = os.path.basename(unmapped1).split('_')[0]
+    output_dir = os.path.join(PARAMS['output_dir'], "aligned")
+
+    logger.info(f"Starting alignment of unmapped reads for sample: {sample_name}")
+
+    # Process each unmapped file separately
+    for unmapped_file, output_bam in [(unmapped1, output_bam1), (unmapped2, output_bam2)]:
+        cmd = (
+            f"bismark "
+            f"--output_dir {output_dir} "
+            f"--genome {PARAMS['reference_genome']} "
+            f"{unmapped_file} "
+            f"--non_directional"
+        )
+
+        subprocess.run(cmd, shell=True, check=True)
+
+        # try:
+        #     # Move the output BAM file to the expected location
+        #     bam_file = os.path.join(output_dir,
+        #                           os.path.basename(unmapped_file).replace('_unmapped_reads_1.fq.gz', '_bismark_bt2.bam')
+        #                           if '_1.' in unmapped_file else
+        #                           os.path.basename(unmapped_file).replace('_unmapped_reads_2.fq.gz', '_bismark_bt2.bam'))
+        #     if os.path.exists(bam_file):
+        #         os.rename(bam_file, output_bam)
+
+        # except subprocess.CalledProcessError as e:
+        #     logger.error(f"Error in align_unmapped for {sample_name}: {str(e)}")
+        #     raise
+
+    logger.info(f"Completed alignment of unmapped reads for sample: {sample_name}")
 
 def main():
     """Main pipeline execution"""
@@ -209,10 +269,8 @@ def main():
     }
 
     try:
-        # Run the pipeline
         pipeline_run(
-            # target_tasks=[align_bismark],
-            # multiprocess=PARAMS["max_parallel_jobs"], #NOTE: not sure this is needed
+            multiprocess=PARAMS['max_parallel_jobs'],
             **pipeline_options
         )
     except Exception as e:
