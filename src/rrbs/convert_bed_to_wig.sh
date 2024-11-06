f01c91ae9156240e7567090b26ebff33dad3ec37
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-11-06 21:59:57 +0100

    Update script

--- src/rrbs/convert_bed_to_wig.sh
+++ src/rrbs/convert_bed_to_wig.sh
@@ -10,9 +10,40 @@ parallel -j 8 '[[ ! -f {.} ]] && mv {} {.}' ::: rrbs_data/*.txt
 # parallel -j 8 'awk '"'"'BEGIN { OFS="\t"; } {if($10=="CpG") print $1, $2, $2+1, $8;}'"'"' {} > {.}_CpG.bg' ::: rrbs_data/*.bed
 LC_COLLATE=C
 
-parallel -j 8 '[[ ! -f {.}_CpG.bg ]] && awk '"'"'BEGIN { OFS="\t"; } {if($10=="CpG") print $1, $2, $2+1, $8;}'"'"' {} | sort -k1,1 -k2,2n > {.}_CpG.bg' ::: rrbs_data/*.bed
-parallel -j 8 '[[ ! -f {.}.bw ]] && bedGraphToBigWig {.}.bg hg19_chrom.sizes {.}.bw' ::: rrbs_data/*_CpG.bg
+# TODO: check if it is important that the header goes missing in the below line
+# parallel -j 8 '[[ ! -f {.}_CpG.bg ]] && awk '"'"'BEGIN { OFS="\t"; } {if($10=="CpG") print $1, $2, $2+1, $8;}'"'"' {} | sort -k1,1 -k2,2n > {.}_CpG.bg' ::: rrbs_data/*.bed
+parallel -j 8 '
+if [[ ! -f {.}_CpG.bedgraph ]]; then
+    HEADER="chromosome\tstart\tend\tmethylation"
+    # First create the content without header
+    awk '"'"'BEGIN { OFS="\t"; }
+        {if($10=="CpG") print $1, $2, $2+1, $8;}'"'"' {} | \
+    sort -k1,1 -k2,2n > {.}_CpG.tmp
 
+    # Add header and content to final file
+    echo -e "$HEADER" > {.}_CpG.bedgraph
+    cat {.}_CpG.tmp >> {.}_CpG.bedgraph
+    rm {.}_CpG.tmp
+fi' ::: rrbs_data/*.bed
+parallel -j 8 '[[ ! -f {.}.bw ]] && bedGraphToBigWig {.}.bedgraph hg19_chrom.sizes {.}.bw' ::: rrbs_data/*_CpG.bedgraph
+
+
+
+for cell_type in Sperm Zygote Oocyte "2-cell" "4-cell" "8-cell" Morula ICM TE; do
+    for meth_type in "CpG"; do
+        # Get all replicates for this condition
+        files=$(ls rrbs_data/*${cell_type}*${meth_type}*.bw)
+        echo "$files"
+        echo
+
+        if [ ${#files[@]} -gt 0 ]; then
+            echo "Processing ${cell_type} ${meth_type}"
+            echo "run command: wiggletools ratio sum ${files[@]} : sum map unit map offset 1 map default -1 ${files[@]} > rrbs_data/${cell_type}_${meth_type}.wig"
+        wiggletools ratio sum "${files[@]}" : sum map unit map offset 1 map default -1 "${files[@]}" > "rrbs_data/${cell_type}_${meth_type}.wig"
+        wig2bed < "rrbs_data/${cell_type}_${meth_type}.wig" > "rrbs_data/${cell_type}_${meth_type}.bed"
+        fi
+    done
+done
 
 # for prefix in "${prefixes[@]}"; do
 # done
