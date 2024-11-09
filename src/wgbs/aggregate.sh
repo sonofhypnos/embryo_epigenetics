cf3a4d03f5c781583e3b10db0f1aa2abc46628dc
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-11-09 11:01:19 +0100

    Update aggregation script

--- src/wgbs/aggregate.sh
+++ src/wgbs/aggregate.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+
+# Configuration variables
+DATA_DIR="/home/tassilo/repos/embryo_epigenetics"
+INPUT_DIR="$DATA_DIR/data"  # Default input directory
+
+# Change directory to test directory based on arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --test)
+            INPUT_DIR="${INPUT_DIR}_test"
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
 # Process each cell type and methylation type separately
 for cell_type in Sperm Oocyte Zygote "2cell" "4cell" "8cell" Morula ICM TE hESC "alpha-Amanitin"; do
     for meth_type in "ACG.TCG" "GCA.GCC.GCT"; do
         # Get all replicates for this condition
-        files=($(ls *${cell_type}*${meth_type}*.bw))
-        echo $files
+        files=("$(ls ${INPUT_DIR}/*${cell_type}*${meth_type}*.bw)")
+        echo "$files"
         
         if [ ${#files[@]} -gt 0 ]; then
             echo "Processing ${cell_type} ${meth_type}"
-	    echo "run command: wiggletools ratio sum ${files[@]} : sum map unit map offset 1 map default -1 ${files[@]} > ${cell_type}_${meth_type}.wig"
-	    wiggletools ratio sum "${files[@]}" : sum map unit map offset 1 map default -1 "${files[@]}" > "${cell_type}_${meth_type}.wig"
-	    wig2bed < "${cell_type}_${meth_type}.wig" > "${cell_type}_${meth_type}.bed"
+	    echo "run command: wiggletools ratio sum ${files[@]} : sum map unit map offset 1 map default -1 ${files[@]} > ${INPUT_DIR}/${cell_type}_${meth_type}.wig"
+	    wiggletools ratio sum "${files[@]}" : sum map unit map offset 1 map default -1 "${files[@]}" > "${INPUT_DIR}/${cell_type}_${meth_type}.wig"
+	    wig2bed < "${INPUT_DIR}/${cell_type}_${meth_type}.wig" > "${INPUT_DIR}/${cell_type}_${meth_type}.bed"
         fi
     done
 done
