11511b93e86019b3a92e6fcecb6d0d04a1e71865
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2025-03-20 11:00:44 +1100

    Update liftover script with correct paths

--- src/liftover_bw.sh
+++ src/liftover_bw.sh
@@ -10,7 +10,9 @@
 #============================================================================
 
 #!/bin/bash
+project_dir="/home/tassilo/embryo_epigenetics"
 
+data_dir="$project_dir/data"
 # Check if a filename was provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <wigfile>"
@@ -32,14 +34,14 @@ BASENAME=$(basename "$WIGFILE" | sed 's/\.[^.]*$//')
 # Convert WIG to bedGraph, sort, and perform liftOver
 wiggletools write_bg - "$WIGFILE" |
     sort -k1,1 -k2,2n >temp.bedGraph &&
-    liftOver temp.bedGraph ../hg19ToHg38.over.chain temp_lifted.bedGraph "${BASENAME}_unlifted.txt" &&
+    liftOver temp.bedGraph "$data_dir/hg19ToHg38.over.chain" temp_lifted.bedGraph "${BASENAME}_unlifted.txt" &&
     sort -k1,1 -k2,2n temp_lifted.bedGraph >temp_lifted_sorted.bedGraph || echo "Something went wrong in the conversion"
 
 # Merge overlapping regions using bedtools
 bedtools merge -i temp_lifted_sorted.bedGraph -c 4 -o mean >temp_lifted_merged.bedGraph
 
 # Convert merged bedGraph to bigWig
-bedGraphToBigWig temp_lifted_merged.bedGraph ../hg38.chrom.sizes "${BASENAME}_hg38.bw"
+bedGraphToBigWig temp_lifted_merged.bedGraph "$data_dir/hg38.chrom.sizes" "${BASENAME}_hg38.bw"
 
 # Clean up temporary files
 rm temp.bedGraph temp_lifted.bedGraph temp_lifted_sorted.bedGraph temp_lifted_merged.bedGraph
